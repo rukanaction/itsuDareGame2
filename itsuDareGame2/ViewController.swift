6e9979817315d28e6cd948d21351e64902487fa9
rukanaction <ruka.34ever57@gmail.com>
2021-02-09 15:33:06 +0900

    Initial Commit

---
+++ itsuDareGame2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  itsuDareGame2
+//
+//  Created by Ruka Saito on 2021/02/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
